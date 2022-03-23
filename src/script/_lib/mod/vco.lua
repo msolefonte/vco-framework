0dea62db5309aef811486d0689a0816b55699398
Marc Solé <msolefonte@gmail.com>
2022-03-23 13:23:47 +0000

    Update v2.0.0
    - Added support for Total War: WARHAMMER III
    - Added changes in the API to simplify submods
    - Compatibility added with the Realms of Chaos submod
    - New routes introduced

---
+++ src/script/_lib/mod/vco.lua
@@ -0,0 +1,28 @@
+local vco = {};
+
+function vco:get_faction_keys()
+    return ipairs(cm:get_human_factions());
+end
+
+function vco:trigger_faction_missions(mod_name, faction_key)
+    local campaign_name = cm:get_campaign_name();
+
+    local status, missions = pcall(require, "script/"..mod_name.."/"..campaign_name.."/"..faction_key.."/missions")
+    if status then
+        for _, mission in ipairs(missions) do
+            if mission and mission ~= "" then
+                cm:trigger_custom_mission_from_string(faction_key, mission);
+            end
+        end
+    end
+end
+
+function vco:trigger_custom_missions(mod_name)
+    if cm:is_new_game() then
+        for _, faction_key in vco:get_faction_keys() do
+            vco:trigger_faction_missions(mod_name, faction_key);
+        end
+    end
+end
+
+core:add_static_object("vco", vco);
